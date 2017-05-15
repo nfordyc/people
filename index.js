527f38ee019d02347681438c4fe86c3830bf98d1
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-15 15:52:27 -0400

    Use create Element and appendchild

---
+++ index.js
@@ -0,0 +1,14 @@
+const personForm = document.querySelector('form')
+
+personForm.onsubmit = ev => {
+    ev.preventDefault()
+    const form = ev.target
+    const details = document.querySelector('.details')
+
+    const personName = form.personName.value
+
+    const em = document.createElement('em')
+    em.textContent = personName
+
+    details.appendChild(em)
+}
