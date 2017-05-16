c56211c9ccb2a67bc4afef23f1b9555c2d0da3ea
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-16 09:36:45 -0400

    Looped over form items to create list

--- index.js
+++ index.js
@@ -8,31 +8,50 @@ const renderColor = (hairColor) => {
     return colorDiv
 }
 
+//make the italized name
+const createName = (personName) => {
+    const name = document.createElement('em')
+    name.textContent = personName
+    return name
+}
+
 const handleSubmit = ev => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
 
-    const personName = form.personName.value
-    const hairColor = form.hairColor.value
+    //get items from the form
+    const formItems = form.elements;
 
-    const em = document.createElement('em')
-    em.textContent = personName;
-
-    const colorDiv = renderColor(hairColor)
-
+    //create unordered list
     const ul = document.createElement('ul')
-    const item1 = document.createElement('li')
-    const item2 = document.createElement('li')
-    
-    item1.textContent = form.age.value
-    item2.textContent = form.birthplace.value
-
-    details.appendChild(em)
-    details.appendChild(colorDiv)
     details.appendChild(ul)
-    ul.appendChild(item1)
-    ul.appendChild(item2)
+
+    let newElement
+
+    //add elements from form into unordered list
+    for(let i = 0;i < formItems.length - 1;i++){
+        newElement = document.createElement('li')
+        switch(formItems[i].name) { //set content of list item based on name
+            case 'hairColor' :
+                newElement.textContent = 'Color:' 
+                newElement.appendChild(renderColor(formItems[i].value))
+                break;
+            case 'personName' :
+                newElement.textContent = 'Name: ' 
+                newElement.appendChild(createName(formItems[i].value))
+                break;
+            case 'age' :
+                newElement.textContent = `Age: ${formItems[i].value}`
+                break;
+            case 'birthplace' :
+                newElement.textContent = `BirthPlace: ${formItems[i].value}`
+                break;
+        }
+        
+        ul.appendChild(newElement)
+    }
+
 }
 
 personForm.addEventListener('submit',handleSubmit)
